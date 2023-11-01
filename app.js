8fbdbe2a92108263a9ce70ad461b7665da3a305f
Nicolas Meuwly <nicomeuwly@mbp-de-nico.einet.ad.eivd.ch>
2023-11-01 10:24:30 +0100

    move mongoose.connect line

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.DATABASE_URL);
-
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL);
 
 app.use(logger("dev"));
 app.use(express.json());
