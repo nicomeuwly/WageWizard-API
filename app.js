fc1b910f0fdbf69dab00027139ccfe64adb24e57
Nicolas Meuwly <nicomeuwly@mbp-de-nico.einet.ad.eivd.ch>
2023-11-01 11:10:45 +0100

    Some changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express();
 
